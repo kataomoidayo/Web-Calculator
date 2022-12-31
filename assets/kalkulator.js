67e32ae8142540982047a17cc26a5a7aac689a47
kataomoiyo <114056087+kataomoiyo@users.noreply.github.com>
2022-12-31 13:54:18 +0800

    コメントを削除した

--- assets/kalkulator.js
+++ assets/kalkulator.js
@@ -1,15 +1,10 @@
-// Kita gunakan objek ini sebagai tempat menyimpan data dan kondisi pada calculator, 
-// di mana angka yang muncul pada layar kalkulator selalu diambil dari data calculator.displayNumber.
-// Nilai dari properti operator dan firstNumber diberikan null dulu karena akan diberikan nilai ketika pengguna melakukan aksi.
-// Properti isWaitForSecondNumber merupakan kondisi di mana kalkulator sedang menunggu pengguna menentukkan angka kedua dalam melakukan perhitungan.
 const calculator = {
     displayNumber: '0',
     operator: null,
     firstNumber: null,
     isWaitForSecondNumber: false,
 };
-// Setelah membuat object calculator, selanjutnya kita buat fungsi-fungsi umum yang dilakukan kalkulator 
-// seperti me-update angka pada layar dan menghapus data pada kalkulator.
+
 function updateDisplay() {
     document.querySelector('#displayNumber').innerText = calculator.displayNumber;
 }
@@ -19,13 +14,7 @@ function clearCalculator() {
     calculator.firstNumber = null;
     calculator.isWaitForSecondNumber = false;
 }
-// Lalu, kita buat juga fungsi untuk memasukkan angka ke dalam nilai 
-// displayNumber kalkulator.
-// Saat ini kalkulator masih dapat menampilkan angka 0 di awal bilangan, 
-// hal itu tentu aneh dan tidak pernah terjadi pada kalkulator manapun, kecuali menampilkan bilangan desimal. 
-// Untuk memperbaikinya, tambahkan sebuah kondisi dimana jika displayNumber bernilai ‘0’ di fungsi inputDigit() 
-// sehingga angka yang pertama dimasukkan pengguna akan menggantikan keseluruhan nilai displayNumber. 
-// Selain itu, lakukan seperti biasanya. Untuk melakukannya kita gunakan if-else statement.
+
 function inputDigit(digit) {
     if (calculator.displayNumber === '0') {
         calculator.displayNumber = digit;
@@ -45,7 +34,7 @@ function handleOperator(operator) {
         calculator.isWaitForSecondNumber = true;
         calculator.firstNumber = calculator.displayNumber;
 
-        // mengatur ulang nilai display number supaya tombol selanjutnya dimulai dari angka pertama lagi
+       
         calculator.displayNumber = '0';
     } else {
         alert('Operator sudah ditetapkan');
@@ -64,7 +53,7 @@ function performCalculation() {
         result = parseInt(calculator.firstNumber) - parseInt(calculator.displayNumber)
     }
   
-    // objek yang akan dikirimkan sebagai argumen fungsi putHistory()
+   
     const history = {
         firstNumber: calculator.firstNumber,
         secondNumber: calculator.displayNumber,
@@ -76,18 +65,13 @@ function performCalculation() {
     renderHistory();
  }
 
-// Kemudian, kita buat variabel buttons dengan menginisialisasikan nilai seluruh elemen button yang ada 
-// dan berikan event click pada tiap elemennya.
 
-// Untuk menangkap semua elemen div.button 
-// kita gunakan querySelectorAll("div.button") dan kita looping nilainya 
-// untuk diberikan event click di setiap elemen button - nya.
 const buttons = document.querySelectorAll('.button');
 
-//ini for of
+
 for (const button of buttons) {
     button.addEventListener('click', function (event) {
-        // Mendapat objek elemen yang diklik
+       
         const target = event.target;
 
         if (target.classList.contains('clear')) {
